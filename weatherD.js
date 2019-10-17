7dd0c5260c4352b6703f05cbaa22f6f659bd9aef
kevin-on-davis <kevin.on.davis@gmail.com>
2019-10-16 21:40:00 -0400

    Initial commit - issue with getting user location

--- weatherD.js
+++ weatherD.js
@@ -1,20 +1,30 @@
 
 
 function getMyLocationURL() {
+    // var longitude = -79.6614656;
+    // var latitude = 43.5986432;
     var longitude;
     var latitude;
 
-    if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(function()
+    if (navigator.geolocation) 
+    {
+        navigator.geolocation.getCurrentPosition(success, error);
+        
+        function success(position)
         {
-            alert("Checking location "+response.data);
-            var position = response.data;
-            alert(position);
-            longitude = position.coords.longitude;
-            latitude = position.coords.latitude;
-        });
-    return `api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}`;
-  };
+            alert("Checking location "+position.coords);
+            var pos = position.coords;
+            longitude = pos.longitude;
+            latitude = pos.latitude;
+        };
+
+        function error(err) 
+        {
+            console.warn(`ERROR(${err.code}): ${err.message}`);
+        }
+          
+        return `https://api.openweathermap.org/data/2.5/weather?&lat=${latitude}&lon=${longitude}&appid=35b3e9dd48656fb03d90eff0aff2cd0c`;
+    };
 };
 
 function getWeatherConditions(queryURL)
