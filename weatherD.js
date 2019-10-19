f444347e08ef03aec06dcb5183fefa3f18231dd1
kevin-on-davis <kevin.on.davis@gmail.com>
2019-10-18 22:06:22 -0400

    Final version - added optional features (avoid duplicates, UV index forecast)

--- weatherD.js
+++ weatherD.js
@@ -5,7 +5,19 @@ var lst_city_list = $("#city_list");
 
 btn_city_search.on("click", function()
 {
+    event.preventDefault();
     getLocationURL();
+});
+
+$("#city_search")
+
+$('input[type=text]').on('keydown', function() 
+{
+    if (event.which == 13) 
+    {
+        event.preventDefault();
+        getLocationURL();
+    }
 });
 
 lst_city_list.on("click", "option", function(event)
@@ -17,7 +29,6 @@ function getLocationURL() {
     var longitude;
     var latitude;
 
-    // debugger;
     showHistory();
     {
         if (!$("#city_search").val())
@@ -52,9 +63,10 @@ function getWeatherConditions(queryURL)
     $.get(queryURL).then(function(res)
     {
         var myWeather = res;
-        var today = moment(Date()).format('LL');
+        var today = moment(Date(1571444065*1000)).format('L');
+
         $("#current_weather").empty();
-        // console.log(res.coord.lat);
+
         var myWeatherIcon = `http://openweathermap.org/img/w/${myWeather.weather[0].icon}.png`;
         $("#current_weather").append(`<h3 class="col-12" style="color:black"><strong>${myWeather.name} (${today}</strong>)<img id="wicon" src="${myWeatherIcon}" alt="Weather icon"></h3>`);
         $("#current_weather").append(`<h6 class="col-12">Temperature: ${myWeather.main.temp} °C</h6>`);
@@ -70,25 +82,31 @@ function getWeatherConditions(queryURL)
         $.get(forecast_URL).then(function(res)
         {
             myForecast = res;
-            // console.log(res);
-            
-            
+
             $("#five_day").empty();
+            currDate = new Date();
+            myStart = Math.floor(currDate.getHours()/3);
             for (i=0; i < 5; i++)
             {
-                var weather_icon_5day = `https://openweathermap.org/img/w/${myForecast.list[i].weather[0].icon}.png`;
-                $("#five_day").append(`<div class="card bg-primary"><div class="card-body text-left" id="day${i}_forecast" style="color:white"><p class="card-text">${moment(moment(today).add(i+1, 'day')).format('LL')}</p></div></div>`);
-                $(`#day${i}_forecast`).append(`<h6>Temp: ${myForecast.list[i].main.temp} °C</h6>`);
+                var weather_icon_5day = `https://openweathermap.org/img/w/${myForecast.list[myStart].weather[0].icon}.png`;
+                $("#five_day").append(`<div class="card bg-primary"><div class="card-body text-left" id="day${i}_forecast" style="color:white"><p class="card-text">${moment(myForecast.list[myStart].dt_txt).format('L')}</p></div></div>`);
+                $(`#day${i}_forecast`).append(`<h6>Temp: ${myForecast.list[myStart].main.temp} °C</h6>`);
                 $(`#day${i}_forecast`).append(`<h6><img id="wicon" src="${weather_icon_5day}" alt="Weather icon">`);
-                $(`#day${i}_forecast`).append(`<h6>Humidity: ${myForecast.list[i].main.humidity} %</h6>`);
+                $(`#day${i}_forecast`).append(`<h6>Humidity: ${myForecast.list[myStart].main.humidity} %</h6>`);
+                $(`#day${i}_forecast`).append(`<h6>Wind: ${myForecast.list[myStart].wind.speed} MPH</h6>`);
 
-                // $.get(`https://api.openweathermap.org/data/2.5/uvi/forecast?lat=${myForecast.city.coord.lat}&lon=${myForecast.city.coord.lon}&appid=${api_key}`).then(function(fore_5d)
-                // {
-                //     console.log(fore_5d[i].value);
-                //     $(`#day${i}_forecast`).append(`<h6>UV Index: ${fore_5d[i].value}</h6>`);
-                // });
+                myStart += 8;
             };
             $("#five_day").prepend(`<h3 class="col-12" style="color:black"><strong>5 Day Forecast</strong></h3>`);
+        }).then(function()
+        {
+            $.get(`https://api.openweathermap.org/data/2.5/uvi/forecast?lat=${myForecast.city.coord.lat}&lon=${myForecast.city.coord.lon}&appid=${api_key}`).then(function(fore_5d)
+            {
+                for (i=0; i < 5; i++)
+                {
+                    $(`#day${i}_forecast`).append(`<h6>UV Index: <span style="color: white; border:solid; border-color:red; border-width:thick; background-color:red">${fore_5d[i].value}</span></h6>`);
+                }
+            });
         });
     });
 }
@@ -106,8 +124,11 @@ function showHistory()
 
     if ($("#city_search").val())
     {
-        srch_history.push($("#city_search").val());
-        localStorage.setItem("city_srch_history", JSON.stringify(srch_history));
+        if (!srch_history.includes($("#city_search").val()))
+        {
+            srch_history.push($("#city_search").val());
+            localStorage.setItem("city_srch_history", JSON.stringify(srch_history));
+        }
     };
 
     $("#city_list").empty();
@@ -119,5 +140,3 @@ function showHistory()
 };
 
 $(document).ready(getLocationURL());
-
-// getMyLocationURL();
